11e341d9df2a739273ada8df74dfbfefc83eaddc
owusuvictorjnr <owusuvictor80@gmail.com>
2022-11-09 01:18:01 +0000

    map data, shop and shopItem

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Layout from "../components/Layout";
+import ShopItem from "../components/ShopItem";
+import data from "../utils/data";
 
 const Home = () => {
   return (
-
-    <Layout title='Home Page'> </Layout>
-  )
+    <Layout title="Home Page">
+      <div className="grid grid-cols-1 gap-4  md:grid-cols-3 lg:grid-cols-4">
+        {data.shops.map((shop) => (
+          <ShopItem shop={shop} key={shop.slug}></ShopItem>
+        ))}
+      </div>
+    </Layout>
+  );
 };
 
 export default Home;
