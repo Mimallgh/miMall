26069cc3d7ce5b301663fb057cc7bd87f202263a
owusuvictorjnr <owusuvictor80@gmail.com>
2022-11-09 01:20:07 +0000

    create json data for shop

---
+++ utils/data.js
@@ -0,0 +1,77 @@
+const data = {
+  shops: [
+    {
+      name: "God is good",
+      slug: "Provisions",
+      image: "/images/pic1.jpg",
+      rating: "4.5",
+      nemReviews: "9",
+      location: "cape coast",
+      description: "a popular shop",
+    },
+
+    {
+      name: "Asempa Shop",
+      slug: "Books",
+      image: "/images/pic2.jpg",
+      rating: "3.5",
+      nemReviews: "7",
+      description: "we sell all kinds of books",
+    },
+
+    {
+      name: "Vitech Solutions",
+      slug: "Hardware & Software Developer",
+      image: "/images/pic3.jpg",
+      rating: "9",
+      nemReviews: "8",
+      description: "We are into all kinds of Software & Hardware Development",
+    },
+
+    {
+      name: "Code Coast",
+      slug: "Software Development Institude",
+      image: "/images/pic4.jpg",
+      rating: "9.1",
+      nemReviews: "9",
+      description: "We train people to become software devs",
+    },
+
+    {
+      name: "Hubtel",
+      slug: "Delivering Service",
+      image: "/images/pic5.jpg",
+      nemReviews: "8",
+      rating: "8.4",
+      description: "24/7 delivering across Ghana",
+    },
+
+    {
+      name: "Mama Akosua Pharmacy",
+      slug: "Pharmacy",
+      image: "/images/pic6.jpg",
+      rating: "9",
+      nemReviews: "7",
+      description: "We sell all kinds of drugs",
+    },
+
+    {
+      name: "Akanayo",
+      slug: "Computers",
+      image: "/images/pic1.jpg",
+      nemReviews: "6",
+      rating: "6.6",
+      description: "we sell all kinds of computers",
+    },
+
+    {
+      name: "Awo's Collection",
+      slug: "Boutique",
+      image: "/images/pic2.jpg",
+      rating: "8.3",
+      nemReviews: "7",
+      description: "We sell all kinds of clothes",
+    },
+  ],
+};
+export default data;
