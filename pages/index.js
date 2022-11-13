bc5529af55c6b14dad5498e66201fa4681166d4f
owusuvictorjnr <owusuvictor80@gmail.com>
2022-11-13 16:05:11 +0000

    call shopApi

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,17 @@ import React from "react";
 import Layout from "../components/Layout";
 import ShopItem from "../components/ShopItem";
 import data from "../utils/data";
+import shopApi from "./api/shopApi";
 
 const Home = () => {
   return (
     <Layout title="Home Page">
-      <div className="grid grid-cols-1 gap-4  md:grid-cols-3 lg:grid-cols-4">
+      <shopApi />
+      {/* <div className="grid grid-cols-1 gap-4  md:grid-cols-3 lg:grid-cols-4">
         {data.shops.map((shop) => (
           <ShopItem shop={shop} key={shop.slug}></ShopItem>
         ))}
-      </div>
+      </div> */}
     </Layout>
   );
 };
