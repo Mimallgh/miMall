2ce4cce8fd8bfef44377c268f5d9b5f08c103f6b
owusuvictorjnr <owusuvictor80@gmail.com>
2022-11-18 04:55:25 +0000

    Nav bar

---
+++ components/Nav.js
@@ -0,0 +1,134 @@
+import React from "react";
+import Link from "next/link";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { BiHelpCircle } from "react-icons/bi";
+import { VscAccount } from "react-icons/vsc";
+import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
+
+const Nav = () => {
+  return (
+    <>
+      <header>
+        {/* To sell on the site */}
+        <div className="sell text-1xl">
+          <Link href="/sell">Sell on MiMall</Link>
+        </div>
+
+        <nav className="h-[5rem] z-20 mt-8 top-0 fixed w-screen shadow-md  bg-green-600 px-4 hidden md:flex justify-between items-center">
+          <Link
+            href="/"
+            className="px-2 py-4 text-[#380648] font-bold text-3xl font-serif"
+          >
+            MiMall
+          </Link>
+
+          <form>
+            <div className="flex">
+              {/* <label
+                  htmlfor="search-dropdown"
+                  className="mb-2 text-sm font-medium sr-only"
+                >
+                  Your Email
+                </label> */}
+              <button
+                id="dropdown-button"
+                data-dropdown-toggle="dropdown"
+                className="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm text-[#ffff] font-medium text-center border border-gray-300 rounded-l-lg"
+                type="button"
+              >
+                <DropdownMenu.Root>
+                  <DropdownMenu.Trigger>All categories</DropdownMenu.Trigger>
+
+                  <DropdownMenu.Content
+                    className="drop-down font-serif text-3xl"
+                    sideOffset={5}
+                  >
+                    <DropdownMenu.Arrow className="text-red-500" />
+                    <DropdownMenu.Item>
+                      <Link href="phones">Mobile Phones</Link>
+                    </DropdownMenu.Item>
+
+                    <DropdownMenu.Item>
+                      <Link href="/laptop">Laptops</Link>
+                    </DropdownMenu.Item>
+
+                    <DropdownMenu.Item>
+                      <Link href="/provisions">Provisions</Link>
+                    </DropdownMenu.Item>
+
+                    <DropdownMenu.Item>
+                      <Link href="/cosmetics">Cosmestics</Link>
+                    </DropdownMenu.Item>
+                  </DropdownMenu.Content>
+                </DropdownMenu.Root>
+              </button>
+
+              <div className="relative w-full">
+                <input
+                  type="search"
+                  id="search-dropdown"
+                  className="block p-2.5  z-20 text-sm text-gray-900  rounded-r-lg border-l-2 border w-[20rem] border-gray-50"
+                  placeholder="Search"
+                  required=""
+                />
+                <button
+                  type="submit"
+                  className="absolute top-0 right-0 p-2.5 text-sm font-medium text-white bg-blue-500 rounded-r-lg border-blue-400 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
+                >
+                  <svg
+                    aria-hidden="true"
+                    className="w-5 h-5"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                    ></path>
+                  </svg>
+                  <span className="sr-only">Search</span>
+                </button>
+              </div>
+            </div>
+          </form>
+
+          <ul className="flex gap-10">
+            <Link
+              href="/account"
+              className="capitalize gap-2 flex text-[#ffff] text-2lg tracking-wide font-serif"
+            >
+              <VscAccount className="flex justify-between " size="25" />
+              account
+            </Link>
+
+            <Link
+              href="/help"
+              className="capitalize gap-2 flex text-[#ffff] text-2lg tracking-wide font-serif"
+            >
+              <BiHelpCircle className="flex" size="25" />
+              help
+            </Link>
+
+            <Link
+              href="/cart"
+              className="capitalize gap-2 flex text-[#ffff] text-2lg tracking-wide font-serif"
+            >
+              <AiOutlineShoppingCart
+                className="flex justify-between"
+                size="25"
+              />
+              cart
+            </Link>
+          </ul>
+          {/* <div>cart login</div> */}
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default Nav;
