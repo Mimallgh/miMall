b1d18182fcd09cfe7012118e1dd9f25acbebe736
owusuvictorjnr <owusuvictor80@gmail.com>
2022-11-18 12:55:57 +0000

    Footer menus

---
+++ components/footer/Menus.js
@@ -0,0 +1,35 @@
+export const CATEGORIES = [
+  { name: "Computers", link: "#" },
+  { name: "Cosmetics", link: "#" },
+  { name: "Electronics", link: "#" },
+  { name: "Provisions", link: "#" },
+  { name: "Accessories", link: "#" },
+];
+
+export const RESOURCES = [
+  { name: "Industries and tools", link: "#" },
+  { name: "Use Cases", link: "#" },
+  { name: "Blog", link: "#" },
+  { name: "Online events", link: "#" },
+];
+
+export const COMPANY = [
+  { name: "About us", link: "#" },
+  { name: "Customer Stories", link: "#" },
+  { name: "Online Communities", link: "#" },
+  //   { name: "About us", link: "#" },
+];
+
+export const SUPPORT = [
+  { name: "Documentation", link: "#" },
+  { name: "Tutorials and guides", link: "#" },
+  { name: "Webinars", link: "#" },
+];
+
+export const Icons = [
+  { name: "logo-facebook", link: "#" },
+  { name: "logo-linkedin", link: "#" },
+  { name: "logo-twitter", link: "#" },
+  { name: "logo-github", link: "#" },
+  { name: "logo-instagram", link: "#" },
+];
