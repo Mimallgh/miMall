29303f994c1d56a01de86ef779f75b57653d9d2b
owusuvictorjnr <owusuvictor80@gmail.com>
2022-11-24 05:53:35 +0000

    Sliding Text

---
+++ components/TextSliders.js
@@ -0,0 +1,23 @@
+import React from "react";
+import TextSlider from "text-slider";
+
+const TextSliders = () => {
+  const slideText = [
+    "Welcome to React Native Text Slider",
+    "It is very easy to use",
+    "You can simply install and use it now",
+    "If you have any issues open up an issue in issues tab",
+  ];
+  return(
+
+   <div>
+    <TextSlider
+      frequency={5000}
+    slideText={slideText}
+    
+    />
+  </div>
+  )
+};
+
+export default TextSliders;
